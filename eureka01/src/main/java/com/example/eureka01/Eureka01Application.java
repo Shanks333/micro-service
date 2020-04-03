21a8cae7107b8012b6e7d3ce484b145f212dbe7c
Shanks333 <982974652@qq.com>
2020-04-03 16:41:07 +0800

    创建注册中心1

---
+++ eureka01/src/main/java/com/example/eureka01/Eureka01Application.java
@@ -0,0 +1,13 @@
+package com.example.eureka01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eureka01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka01Application.class, args);
+    }
+
+}
