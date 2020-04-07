00dbcaf19d16e859fe0119b08afc9b1611815dbd
Shanks333 <982974652@qq.com>
2020-04-07 16:57:37 +0800

    修改学生服务的配置文件

---
+++ log-server/src/main/java/com/example/logserver/domain/StuLog.java
@@ -0,0 +1,22 @@
+package com.example.student.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author Shanks
+ * @version 1.0
+ * @program com.example.student.mapper
+ * @description 操作学生信息的日志
+ * @date 2020/4/5 15:09
+ */
+@Data
+public class StuLog implements Serializable {
+
+    private Integer id;
+
+    private String username;
+
+    private String desc;
+}
